f77ab010c7149a54957097cdf963306a90d8e04b
kariminf <kariminfo0@gmail.com>
2018-07-06 21:41:55 +0100

    php exceptions handling

---
+++ php/except.php
@@ -0,0 +1,40 @@
+#!/usr/bin/php
+<?php
+
+class NegException extends Exception {
+  public function errorMessage() {
+    return "Negative numbers don't have a factorial!";
+  }
+}
+
+class BigException extends Exception {
+  public function errorMessage() {
+    return "The number is too big!";
+  }
+}
+
+function fact($i) {
+  if ($i < 0) throw new NegException();
+  if ($i > 5) throw new BigException();
+  if ($i <= 1) return 1;
+	return $i * fact($i - 1);
+}
+
+if (sizeof($argv) < 2) {
+  echo "Please enter an integer value\n";
+  exit();
+}
+
+$n = $argv[1];
+
+try {
+  $f = fact($n);
+  echo "Fact($n)= " . $f . "\n";
+}
+catch (Exception $e) {
+  echo $e->errorMessage() . "\n";
+}
+catch (BigException $e) {
+  echo $e->errorMessage() . "\n";
+}
+?>
