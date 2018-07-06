3bc32c2d47fb8ca8f09ef8d7aeb7e7264feb7beb
kariminf <kariminfo0@gmail.com>
2018-07-06 18:40:40 +0100

    lua exceptions handling

--- REF.md
+++ REF.md
@@ -21,6 +21,7 @@
 * https://www.lua.org/pil/16.html
 * http://lua-users.org/wiki/ObjectOrientationTutorial
 * http://lua-users.org/wiki/ObjectOrientedProgramming
+* https://www.lua.org/pil/8.4.html
 
 ## Perl
 
---
+++ lua/except.lua
@@ -0,0 +1,21 @@
+function fact(i)
+	if i < 0 then error "Negative numbers don't have a factorial!" end
+	if i > 5 then error "The number is too big!" end
+
+	if i == 0 then return 1 end
+	return i * fact(i-1)
+end
+
+if #arg < 1 then
+	print ("Please enter an integer value")
+	os.exit()
+end
+
+n = tonumber(arg[1])
+
+success, f = pcall(fact, n)
+if success then
+	print ("Fact(" .. n .. ")= " .. f)
+else
+	print (f)
+end
