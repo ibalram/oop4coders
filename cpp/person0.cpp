061198fae3df6736d358d9cafc58d1755a83537f
kariminf <kariminfo0@gmail.com>
2018-07-07 20:06:54 +0100

    c++ class def

--- cpp/person0.cpp
+++ cpp/person0.cpp
@@ -3,25 +3,49 @@
 
 class Person
 {
-    public:
 
-        Person(std::string name, int byeay){
-            this->name = name;
-            this->byear = byear;
-        };
+  public:
 
-        void info() {
-            std::cout << "My name: " << this->name << "\n";
-            std::cout << "My birth year: " << this->byear << "\n";
-        };
-    private:
-        std::string name;
-        int byear;
+    Person(std::string name, int byear){
+      this->name = name;
+      this->byear = byear;
+      nbr++;
+    };
+
+    ~Person(){
+      nbr--;
+      std::cout << name << " is out\n";
+    };
+
+    void info() {
+      std::cout << "My name: " << name ;
+      std::cout << ", My birth year: " << this->byear << "\n";
+    };
+
+    static int population() {
+      return nbr;
+    };
+
+  private:
+
+    std::string name;
+    int byear;
+    static int nbr;
 };
 
+int Person::nbr = 0;
 
 int main()
 {
-	Person p = Person("Karim", 1996);
+  Person p = Person("Karim", 1986);
+  Person* p2 = new Person("Karim+1", 1987);
+
   p.info();
+  p2->info();
+  int nbr = Person::population();
+
+  std::cout << "The number of persons: " << nbr << "\n";
+  delete p2; //delete the pointer
+  nbr = Person::population();
+  std::cout << "The number of persons: " << nbr << "\n";
 }
