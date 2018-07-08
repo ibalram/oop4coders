23f12f647d25b2e8748b625699a9ed341e709b91
kariminf <kariminfo0@gmail.com>
2018-07-08 18:21:05 +0100

    lua class def

--- lua/person.lua
+++ lua/person.lua
@@ -2,14 +2,28 @@ local Person = {}
 Person.__index = Person
 
 function Person:new(name, byear)    -- The constructor
+  Person.nbr = Person.nbr + 1
   return setmetatable({name = name, byear = byear}, Person)
 end
 
+Person.nbr = 0
+
 function Person:info()
-  print("My name: " .. self.name)
-  print("My birth year: " .. self.byear)
+  print("My name: " .. self.name .. ", My birth year: " .. self.byear)
+end
+
+function Person.population()
+  return Person.nbr
 end
 
 local p = Person:new("Karim", 1986)
+local p2 = Person:new("Karim+1", 1987)
 
 p:info()
+p2:info()
+local nbr = Person.population()
+
+print("The number of persons: " .. nbr)
+p2 = nil
+nbr = Person.population()
+print("The number of persons: " .. nbr)
