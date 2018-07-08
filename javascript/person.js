d7212050043f459bf4647b061ed263ea541f4462
kariminf <kariminfo0@gmail.com>
2018-07-08 16:22:45 +0100

    javascript class def

--- javascript/person.js
+++ javascript/person.js
@@ -1,14 +1,29 @@
 class Person {
 	constructor(name, byear) {
     this.name = name;
-    this.age = byear;
+    this["byear"] = byear;
+		Person.nbr++;
   }
 
 	info(){
-		console.log("My name: " + this.name);
-		console.log("My birth year: " + this.byear);
+		console.log("My name: " + this.name + ", My birth year: " + this.byear);
+	}
+
+	static population() {
+		return Person.nbr;
 	}
 }
 
+Person.nbr = 0;
+
 var p = new Person("Karim", 1986);
+var p2 = new Person("Karim+1", 1987);
+
 p.info();
+p2.info();
+var nbr = Person.population();
+
+console.log("The number of persons: " + nbr);
+p2 = null;
+nbr = Person.population();
+console.log("The number of persons: " + nbr);
