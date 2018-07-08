2b6993de5b74b19c2e7035fa7ed913235b0a28fc
kariminf <kariminfo0@gmail.com>
2018-07-08 23:18:20 +0100

    ruby class def

--- ruby/person.rb
+++ ruby/person.rb
@@ -2,16 +2,30 @@
 
 class Person
 
+	@@nbr = 0
+
 	def initialize(name, byear)
 		@name, @byear = name, byear
+		@@nbr += 1
 	end
 
-	def info()
-		puts "My name: #@name"
-		puts "My birth year: #@byear"
+	def info
+		puts "My name: #@name, My birth year: #@byear"
+	end
+
+	def self.population
+		@@nbr
 	end
 end
 
 p = Person.new("Karim", 1986)
+p2 = Person.new("Karim+1", 1987)
 
 p.info()
+p2.info()
+nbr = Person.population
+
+puts "The number of persons: #{nbr}"
+p2 = nil
+nbr = Person.population
+puts "The number of persons: #{nbr}"
