210e79d1562157e095e444c0152ec124da9332f7
kariminf <kariminfo0@gmail.com>
2018-07-08 19:37:02 +0100

    php class def

--- php/person.php
+++ php/person.php
@@ -1,19 +1,41 @@
 <?php
 class Person {
-	var $name;
-	var $byear;
-	function __construct( $name, $byear ) {
+
+	private $name;
+	private $byear;
+	private static $nbr = 0;
+
+	public function __construct( $name, $byear ) {
 		$this->name = $name;
 		$this->byear = $byear;
+		self::$nbr++;
 	}
 
-	function info() {
-		echo "My name: $this->name \n";
-		echo "My birth year: $this->byear \n";
+	function __destruct() {
+		self::$nbr--;
+		print "$this->name is out\n";
 	}
+
+	public function info() {
+		echo "My name: $this->name, My birth year: $this->byear \n";
+	}
+
+	public function population() {
+		return self::$nbr;
+	}
+
 }
 
 $p = new Person("Karim", 1986);
+$p2 = new Person("Karim+1", 1987);
+
 $p->info();
+$p2->info();
+$nbr = Person::population();
+
+print "The number of persons: $nbr\n";
+unset($p2);
+$nbr = Person::population();
+print "The number of persons: $nbr\n";
 
 ?>
