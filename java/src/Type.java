cf5b9c161b46e0f259620505766d0d9ab9022c84
kariminf <kariminfo0@gmail.com>
2018-07-09 21:01:50 +0100

    type manipulation should be in polymorphism not in object

---
+++ java/src/Type.java
@@ -0,0 +1,13 @@
+
+public class Type {
+	public static void main(String[] args) {
+		Person karim = new Person();
+		Animal cat = new Animal();
+		
+		System.out.println("variable karim is of type : " + karim.getClass().getName());
+		
+		
+	}
+}
+
+class Animal {}
