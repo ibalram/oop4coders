94339522bc11c4fff7796001970e88849ea15057
kariminf <kariminfo0@gmail.com>
2018-07-10 10:30:15 +0100

    php object methods

---
+++ php/objmeth.php
@@ -0,0 +1,32 @@
+<?php
+class Person {
+
+	private $name;
+	private $byear;
+
+	public function __construct( $name, $byear ) {
+		$this->name = $name;
+		$this->byear = $byear;
+	}
+
+	public function __toString() {
+		return "($this->name: $this->byear)";
+	}
+
+	public function __clone() {
+		$this->byear ++;
+	}
+
+}
+
+$p = new Person("Karim", 1986);
+
+print "As string: $p\n";
+
+$p3 = clone $p;
+print "P3: $p3\n";
+
+$b = ($p==$p3)? "true": "false";
+print "p == p3? " . $b . "\n";
+
+?>
