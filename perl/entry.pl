6ef3d53460ccc29b83f4dc1843ed0ce9ab092093
kariminf <kariminfo0@gmail.com>
2018-07-11 11:28:10 +0100

    perl entry point

---
+++ perl/entry.pl
@@ -0,0 +1,13 @@
+#! /usr/bin/perl -w
+
+my $n = $ARGV[0];
+die "Please enter an integer value" unless (defined $n);
+
+print "Fact($n)= ${\fact($n)}\n";
+print "Fact(" . $n . ")= ". fact($n) . "\n";
+
+sub fact {
+	my $i = shift;
+	return 1 if ($i <= 0);
+	return $i * fact($i - 1);
+}
