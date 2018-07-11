e565e3d23a3e51bd02ae5507703be2702fbf8312
kariminf <kariminfo0@gmail.com>
2018-07-11 10:55:17 +0100

    lua entry point

---
+++ lua/entry.lua
@@ -0,0 +1,12 @@
+fact = function (i)
+	if i <= 0 then return 1 end
+	return i * fact(i-1)
+end
+
+if #arg < 1 then
+	print ("Please enter an integer value")
+	os.exit()
+end
+
+n = tonumber(arg[1])
+print ("Fact(" .. n .. ")= " .. fact(n))
