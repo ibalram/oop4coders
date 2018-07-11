8f9dcf880be1b1942dc407dc915282ae991df48c
kariminf <kariminfo0@gmail.com>
2018-07-11 10:29:31 +0100

    javascript entry point

---
+++ javascript/entry.js
@@ -0,0 +1,14 @@
+var args = process.argv;
+
+if (args.length < 3) {
+	console.log("Please enter an integer value");
+	process.exit();
+}
+
+var n = args[2];
+console.log("Fact(" + n + ")= " + fact(n) );
+
+function fact(i) {
+	if (i <= 0) return 1;
+	return i * fact(i-1);
+}
