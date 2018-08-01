80b8afb24db4db187d6297c75c52d1907e810f54
kariminf <kariminfo0@gmail.com>
2018-08-01 14:46:55 +0100

    object methods: perl

--- codes/perl/objmeth.pl
+++ codes/perl/objmeth.pl
@@ -13,12 +13,30 @@
 		return $self;
 	}
 
-	use overload  '""' => sub {
-		my ($self) = @_;
-		return "($self->{_name}: $self->{_byear})"
-	}
+	use overload
+		'""' => sub {
+			my ($self) = @_;
+			return "($self->{_name}: $self->{_byear})"
+		},
+		'==' => sub {
+			my ($self, $other) = @_;
+			return ($self->{_name} eq $other->{_name})
+		}
 
 }
 
 $p = new Person("Karim", 1986);
-print "As string: : $p\n";
+$p2 = new Person("Karim+1", 1986);
+$p3 = {
+	_name => "Karim",
+	_byear => 1987,
+};
+
+sub boolval {
+	my $bool = shift;
+	return $bool? "true": "false";
+}
+print "As string: $p\n";
+print "As string: " . $p2 . "\n";
+print "p == p2: " . boolval($p == $p2) . "\n";
+print "p == p3: " . boolval($p == $p3) . "\n";
