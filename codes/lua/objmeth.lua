b5f14e70f621c6228279d9a6627c4519619f8d67
kariminf <kariminfo0@gmail.com>
2018-08-01 12:52:06 +0100

    object methods: lua

--- codes/lua/objmeth.lua
+++ codes/lua/objmeth.lua
@@ -9,5 +9,25 @@ function Person:__tostring()
   return "(" .. self.name .. ": " .. self.byear .. ")"
 end
 
+function Person.__eq (p1, p2)
+  return p1.name == p2.name
+end
+
+function Person.__lt (p1, p2)
+  return (p1.byear < p2.byear) or (p1.byear == p2.byear and p1.name < p2.name)
+end
+
+function Person.__le (p1, p2)
+  return (p1.byear < p2.byear) or (p1.byear == p2.byear and p1.name <= p2.name)
+end
+
 local p = Person:new("Karim", 1986)
+local p2 = Person:new("Karim+1", 1986)
+local p3 = {name = "Karim", byear = 1987}
+
 print("As string: " .. tostring(p))
+print("p == p3? " .. tostring(p==p3))
+print("p ~= p3? " .. tostring(p~=p3))
+print("p <= p2? " .. tostring(p <= p2))
+print("p > p2? " .. tostring(p > p2))
+print("p > p3? " .. tostring(p > p3))
