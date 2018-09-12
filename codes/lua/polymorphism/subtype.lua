d0bd42de3f5b536361d51cebf6f6473da411c3ea
kariminf <kariminfo0@gmail.com>
2018-09-12 13:45:07 +0100

    subtype polymorphism: Lua

---
+++ codes/lua/polymorphism/subtype.lua
@@ -0,0 +1,55 @@
+-- Person class
+local Person = {}
+Person.__index = Person
+
+function Person:new()    -- The constructor
+    local instance = {}
+    setmetatable(instance, self) --self, here, will refer to the calling class
+    return instance
+end
+
+function Person:talk()
+    print("I am a person")
+end
+
+-- Student class
+local Student = {}
+Student.__index = Student
+setmetatable(Student, Person) -- extends
+
+-- Professor class
+local Professor = {}
+Professor.__index = Professor
+setmetatable(Professor, Person) -- extends
+
+function announce(msg, talker)
+    io.write(msg .. ": ")
+    if type(talker) == "table" and type(talker.talk) == "function" then
+        talker:talk()
+    else
+        print("Sorry! I do not talk!")
+    end
+end
+
+local pe = Person:new()
+local st = Student:new()
+local pr = Professor:new()
+
+local cat = {
+    talk = function(); print("Meow!"); end
+}
+
+announce("Person", pe)
+announce("Student", st)
+announce("Professor", pr)
+announce("a cat", cat)
+announce("a number", 25)
+
+print("A table with different types")
+local people = {pe, st, pr, cat, 25}
+for i =1, #people do
+    success, res = pcall(function() people[i].talk(); end)
+    if not success then
+        print("The element n°" .. i .. " does not talk()")
+    end
+end
