9f1b92dfde894d5ae091197744dd183d6a5b2939
kariminf <kariminfo0@gmail.com>
2018-09-22 23:04:05 +0100

    type manipulation: C++ not complete

--- codes/cpp/polymorphism/type_manip.cpp
+++ codes/cpp/polymorphism/type_manip.cpp
@@ -4,33 +4,62 @@
 
 class Person {
 public:
-    void talk(){
-        std::cout << "I am a person" << std::endl;
-    }
+    virtual void talk(){ std::cout << "I am a person" << std::endl; }
 };
 
 class Student: public Person {
 public:
-    virtual void study(){
-        std::cout << "I am studying" << std::endl;
-    }
+    void study(){ std::cout << "I am studying" << std::endl; }
 };
 
 class Professor: public Person {
-    void teach(){
-        std::cout << "I am teaching" << std::endl;
-    }
+public:
+    int nbr;
+    Professor(){ nbr = 5; }
+    void teach(){ std::cout << "I am teaching" << std::endl; }
 };
+
+class Machine {};
+
+class Mooc: public Machine, public Professor {};
+
+template<typename Class, typename T>
+inline bool instanceof(const T *ptr) {
+    return dynamic_cast<const Class*>(ptr) != 0;//nullptr with C++11
+}
 
 int main()
 {
-    Person pe;
-    Person st = Student();
-    Person pr = Professor();
+    Person pe; Student st; Professor pr; Mooc mooc;
+    Person *pst = &st;
+    Person *ppr = &pr;
+    Person *pmooc = &mooc;
+    Machine *mmooc = &mooc;
 
-    std::cout << "Type of pe is: " << typeid(pe).name() << std::endl;
-    std::cout << "Type of st is: " << typeid(st).name() << std::endl;
-    std::cout << "Type of pr is: " << typeid(pr).name() << std::endl;
+    std::cout << "==========================" << std::endl;
+    std::cout << "Object type" << std::endl;
+    std::cout << "==========================" << std::endl;
+    std::cout << "typeid(pe): " << typeid(pe).name() << std::endl;
+    std::cout << "typeid(pst): " << typeid(pst).name() << std::endl;
+    std::cout << "typeid(*pst): " << typeid(*pst).name() << std::endl;
+    std::cout << "typeid(ppr): " << typeid(ppr).name() << std::endl;
+    std::cout << "typeid(*ppr): " << typeid(*ppr).name() << std::endl;
+    std::cout << "typeid(pmooc): " << typeid(pmooc).name() << std::endl;
+    std::cout << "typeid(*pmooc): " << typeid(*pmooc).name() << std::endl;
+    std::cout << "typeid(mmooc): " << typeid(mmooc).name() << std::endl;
+    std::cout << "typeid(*mmooc): " << typeid(*mmooc).name() << std::endl;
+
+    std::cout << "==========================" << std::endl;
+    std::cout << "Instance of" << std::endl;
+    std::cout << "==========================" << std::endl;
+    std::cout << "instanceof<Person>(pst): " <<  instanceof<Person>(pst) << std::endl;
+    std::cout << "instanceof<Student>(pst): " <<  instanceof<Student>(pst) << std::endl;
+    std::cout << "instanceof<Professor>(pst): " <<  instanceof<Professor>(pst) << std::endl;
+    std::cout << "instanceof<Person>(ppr): " <<  instanceof<Person>(ppr) << std::endl;
+    std::cout << "instanceof<Student>(ppr): " <<  instanceof<Student>(ppr) << std::endl;
+    std::cout << "instanceof<Person>(pmooc): " <<  instanceof<Person>(pmooc) << std::endl;
+    std::cout << "instanceof<Machine>(pmooc): " <<  instanceof<Machine>(pmooc) << std::endl;
+
 
     return 0;
 }
