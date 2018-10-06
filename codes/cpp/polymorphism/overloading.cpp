86c6260363b312c49362bd3cbfec5a68a541eb8b
kariminf <kariminfo0@gmail.com>
2018-10-06 17:26:26 +0100

    overloading: C++

---
+++ codes/cpp/polymorphism/overloading.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+class Person {
+public:
+    void read(){ std::cout << "I am a reading" << std::endl; }
+    void read(std::string text){ std::cout << "A text: " << text << std::endl; }
+};
+
+class Student: public Person {
+public:
+    using Person::read;
+    void read(int nbr){ std::cout << "I read on table n°: " << nbr << std::endl; }
+};
+
+int main()
+{
+    Person pe;
+    Student st;
+
+    std::cout << "PERSON" << std::endl;
+    pe.read();
+    pe.read("I am a person");
+
+    std::cout << "STUDENT" << std::endl;
+    st.read(); // using Person::read; or error
+    st.Person::read(); // Without using Person::read;
+    st.read("I am a student"); // using Person::read; or error
+    st.read(5);
+
+    return 0;
+}
